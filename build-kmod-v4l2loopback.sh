ff5f9b874842e2b2314355293534c27aceabc9e3
Benjamin Sherman <benjamin@holyarmy.org>
2023-05-28 22:08:30 -0500

    fix: typo in v4l2loop rpm query (#8)

--- build-kmod-v4l2loopback.sh
+++ build-kmod-v4l2loopback.sh
@@ -12,7 +12,7 @@ RELEASE="$(rpm -E '%fedora')"
 ### BUILD v4l2loopbak (succeed or fail-fast with debug output)
 rpm-ostree install \
     akmod-v4l2loopback-*.fc${RELEASE}.${ARCH}
-V4L2LOOP_AKMOD_VERSION="$(basename "$(rpm -q "akmod-v4l2loopback" --queryformat '%{VERSION}-%{RELEASE}.${ARCH}')" ".fc${RELEASE%%.*}")"
+V4L2LOOP_AKMOD_VERSION="$(basename "$(rpm -q "akmod-v4l2loopback" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" ".fc${RELEASE%%.*}")"
 akmods --force --kernels "${KERNEL}" --kmod "v4l2loopback"
 modinfo /usr/lib/modules/${KERNEL}/extra/v4l2loopback/v4l2loopback.ko.xz > /dev/null \
 || (cat /var/cache/akmods/v4l2loopback/${V4L2LOOP_AKMOD_VERSION}-for-${KERNEL}.failed.log && exit 1)
