de8dbed3050de11232472c415d5e1b02a21fe6c2
Kyle Gospodnetich <me@kylegospodneti.ch>
2023-07-31 23:30:53 -0700

    feat: Add evdi kmod to support displaylink

---
+++ build-kmod-evdi.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/negativo17-fedora-multimedia.repo /etc/yum.repos.d/
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+
+### BUILD evdi (succeed or fail-fast with debug output)
+export CFLAGS="-fno-pie -no-pie"
+rpm-ostree install \
+    akmod-evdi-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod evdi
+modinfo /usr/lib/modules/${KERNEL}/extra/evdi/evdi.ko.xz > /dev/null \
+|| (find /var/cache/akmods/evdi/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+unset CFLAGS
