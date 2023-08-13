ad0dfc91eb9d5b6d4fefa6b8941403d3a2b7e10c
Robert Sturla <robertsturla@outlook.com>
2023-08-13 12:32:22 +0100

    don't disable modularity repos on F39+

--- build-prep.sh
+++ build-prep.sh
@@ -7,7 +7,12 @@ set -oeux pipefail
 ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
+# Modularity repositories are not available on Fedora 39 and above, so don't try to disable them
+if [[ "${FEDORA_VERSION}" -le 38 ]]; then
+    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
+else
+    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+fi
 
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
