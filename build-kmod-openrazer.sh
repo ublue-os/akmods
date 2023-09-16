57c91c70cf901ede5287a5b3723136ba7d84b258
Kyle Gospodnetich <me@kylegospodneti.ch>
2023-09-16 07:29:03 -0700

    feat: Add OpenRazer kmod (#63)

---
+++ build-kmod-openrazer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+
+### BUILD openrazer (succeed or fail-fast with debug output)
+rpm-ostree install \
+    akmod-openrazer-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod openrazer
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razerkbd.ko.xz > /dev/null \
+|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razermouse.ko.xz > /dev/null \
+|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razerkraken.ko.xz > /dev/null \
+|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razeraccessory.ko.xz > /dev/null \
+|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
