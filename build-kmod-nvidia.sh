6ac67cfeba846b71075d5ee8336e3871529b646f
RJ Trujillo <eyecantcu@pm.me>
2023-10-21 22:19:21 -0600

    feat(nvidia): Attempt to support NVIDIA open

--- build-kmod-nvidia.sh
+++ build-kmod-nvidia.sh
@@ -22,14 +22,27 @@ rpm-ostree install \
     akmod-${NVIDIA_PACKAGE_NAME}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE} \
     xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda,devel,kmodsrc,power}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE}
 
+if [[ "${NVIDIA_DRIVER_VARIANT}" != "proprietary" ]]; then
+    rpm-ostree install rpmfusion-nonfree-release-tainted
+    rpm-ostree install \
+        akmod-${NVIDIA_PACKAGE_NAME}-${NVIDIA_DRIVER_VARIANT}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE}
+fi
+
 # Either successfully build and install the kernel modules, or fail early with debug output
 KERNEL_VERSION="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-${NVIDIA_PACKAGE_NAME}" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
+if [[ "${NVIDIA_DRIVER_VARIANT}" =~ "proprietary" ]]; then
+    NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-${NVIDIA_PACKAGE_NAME}" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
+else
+    NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-${NVIDIA_PACKAGE_NAME}-${NVIDIA_DRIVER_VARIANT}" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
+fi
 NVIDIA_LIB_VERSION="$(basename "$(rpm -q "xorg-x11-drv-${NVIDIA_PACKAGE_NAME}" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
 NVIDIA_FULL_VERSION="$(rpm -q "xorg-x11-drv-${NVIDIA_PACKAGE_NAME}" --queryformat '%{EPOCH}:%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-
 akmods --force --kernels "${KERNEL_VERSION}" --kmod "${NVIDIA_PACKAGE_NAME}"
+
+if [[ "${NVIDIA_DRIVER_VARIANT}" != "proprietary" ]]; then
+    akmods --force --kernels "${KERNEL_VERSION}" --kmod "${NVIDIA_PACKAGE_NAME}"-"${NVIDIA_DRIVER_VARIANT}"
+fi
 
 modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/${NVIDIA_PACKAGE_NAME}/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
 (cat /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
@@ -43,4 +56,3 @@ NVIDIA_FULL_VERSION=${NVIDIA_FULL_VERSION}
 NVIDIA_AKMOD_VERSION=${NVIDIA_AKMOD_VERSION}
 NVIDIA_LIB_VERSION=${NVIDIA_LIB_VERSION}
 EOF
-
