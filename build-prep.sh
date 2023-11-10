45eb0d0461571680c2c3c8d9c932aea7e3b9f991
RJ Trujillo <eyecantcu@pm.me>
2023-11-10 08:12:45 -0700

    feat: Build akmods for kernel-fsync

--- build-prep.sh
+++ build-prep.sh
@@ -44,6 +44,20 @@ if [[ "asus" == "${KERNEL_FLAVOR}" ]]; then
         kernel-modules \
         kernel-modules-core \
         kernel-modules-extra
+elif [[ "fsync" == "${KERNEL_FLAVOR}" ]]; then
+    echo "Installing kernel-fsync:"
+    wget https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-$(rpm -E %fedora)/sentry-kernel-fsync-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_sentry-kernel-fsync.repo
+    rpm-ostree cliwrap install-to-root /
+    rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:sentry:kernel-fsync \
+        kernel \
+        kernel-core \
+        kernel-devel \
+        kernel-devel-matched \
+        kernel-modules \
+        kernel-modules-core \
+        kernel-modules-extra
 elif [[ "surface" == "${KERNEL_FLAVOR}" ]]; then
     echo "Installing Surface Kernel:"
     # Add Linux Surface repo
