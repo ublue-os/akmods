d0462b3b1fcafc22bbb919ba54dc1f1150dcb54c
RJ Trujillo <eyecantcu@pm.me>
2023-12-27 16:20:50 -0700

    chore(vbox): Remove copr enablement/disablement

--- build-kmod-VirtualBox.sh
+++ build-kmod-VirtualBox.sh
@@ -2,8 +2,6 @@
 
 set -oeux pipefail
 
-cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
-
 ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
@@ -15,5 +13,3 @@ rpm-ostree install \
 akmods --force --kernels "${KERNEL}" --kmod VirtualBox
 modinfo /usr/lib/modules/${KERNEL}/extra/VirtualBox/{vboxdrv,vboxnetadp,vboxnetflt}.ko.xz > /dev/null \
 || (find /var/cache/akmods/VirtualBox/ -name \*.log -print -exec cat {} \; && exit 1)
-
-rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
