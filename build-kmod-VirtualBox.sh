d8f293074d7b1ecbc61918fa0e26379259949575
RJ Trujillo <eyecantcu@pm.me>
2023-12-27 16:38:22 -0700

    feat: Add VirtualBox kmod (#106)

---
+++ build-kmod-VirtualBox.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+
+### BUILD VirtualBox (succeed or fail-fast with debug output)
+rpm-ostree install \
+    akmod-VirtualBox-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod VirtualBox
+modinfo /usr/lib/modules/${KERNEL}/extra/VirtualBox/{vboxdrv,vboxnetadp,vboxnetflt}.ko.xz > /dev/null \
+|| (find /var/cache/akmods/VirtualBox/ -name \*.log -print -exec cat {} \; && exit 1)
