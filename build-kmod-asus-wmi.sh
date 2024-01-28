dc889655e277ddec6727f792156691936841b2d5
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-01-28 15:57:25 -0800

    feat: Add asus-wmi driver patched for ASUS Ally

---
+++ build-kmod-asus-wmi.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+rpm-ostree install \
+    akmod-asus-wmi-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod asus-wmi
+modinfo /usr/lib/modules/${KERNEL}/extra/asus-wmi/asus-wmi.ko.xz > /dev/null \
+|| (find /var/cache/akmods/asus-wmi/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
