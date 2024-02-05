ccc2ecbaea7293bbc74c9818dadf242baf844b80
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-02-04 18:23:08 -0800

    feat: Add ayaneo-platform driver

---
+++ build-kmod-ayaneo-platform.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+rpm-ostree install \
+    akmod-ayaneo-platform-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod ayaneo-platform
+modinfo /usr/lib/modules/${KERNEL}/extra/ayaneo-platform/ayaneo-platform.ko.xz > /dev/null \
+|| (find /var/cache/akmods/ayaneo-platform/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
