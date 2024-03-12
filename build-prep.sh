25fc297f168f42d2c897cc752cfce7afcd52548e
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-03-12 00:34:15 -0700

    fix: Add version gating for the fsync kernel to avoid regressions and allow further testing (#141)

--- build-prep.sh
+++ build-prep.sh
@@ -53,20 +53,20 @@ if [[ "asus" == "${KERNEL_FLAVOR}" ]]; then
         kernel-modules \
         kernel-modules-core \
         kernel-modules-extra
-elif [[ "fsync" == "${KERNEL_FLAVOR}" ]]; then
-    echo "Installing kernel-fsync:"
+elif [[ "${KERNEL_FLAVOR}" =~ "fsync" ]]; then
+    echo "Installing kernel-fsync-${KERNEL_FLAVOR}:"
     wget https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-$(rpm -E %fedora)/sentry-kernel-fsync-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_sentry-kernel-fsync.repo
     rpm-ostree cliwrap install-to-root /
     rpm-ostree override replace \
     --experimental \
     --from repo=copr:copr.fedorainfracloud.org:sentry:kernel-fsync \
-        kernel \
-        kernel-core \
-        kernel-devel \
-        kernel-devel-matched \
-        kernel-modules \
-        kernel-modules-core \
-        kernel-modules-extra
+        kernel-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64 \
+        kernel-core-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64 \
+        kernel-devel-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64 \
+        kernel-devel-matched-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64 \
+        kernel-modules-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64 \
+        kernel-modules-core-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64 \
+        kernel-modules-extra-"${KERNEL_FLAVOR}".fc"${RELEASE}".x86_64
 elif [[ "surface" == "${KERNEL_FLAVOR}" ]]; then
     echo "Installing Surface Kernel:"
     # Add Linux Surface repo
