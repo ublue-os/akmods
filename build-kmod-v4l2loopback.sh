32cb3613191ba012163c7c3109318651f2b86112
Benjamin Sherman <benjamin@holyarmy.org>
2024-03-27 18:05:00 -0500

    fix: 3 kmods now work on F40 (#154)

--- build-kmod-v4l2loopback.sh
+++ build-kmod-v4l2loopback.sh
@@ -7,11 +7,6 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-if [[ "${KERNEL}" =~ "6.8" ]]; then
-  echo "SKIPPED BUILD of v4l2loopback: compile failure on kernel 6.8 as of 2024-03-17"
-  exit 0
-fi
-
 ### BUILD v4l2loopbak (succeed or fail-fast with debug output)
 rpm-ostree install \
     akmod-v4l2loopback-*.fc${RELEASE}.${ARCH}
