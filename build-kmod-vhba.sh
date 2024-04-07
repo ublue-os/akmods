f8e31a9c5583c92696870635257cbcf47d141b67
Marco Rodolfi <marco.rodolfi@tuta.io>
2024-04-07 16:18:50 +0200

    Cleaning up module name

--- build-kmod-vhba.sh
+++ build-kmod-vhba.sh
@@ -8,9 +8,9 @@ KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
 RELEASE="$(rpm -E '%fedora')"
 
 
-### BUILD wl (succeed or fail-fast with debug output)
+### BUILD vhba (succeed or fail-fast with debug output)
 rpm-ostree install \
     akmod-vhba-*.fc${RELEASE}.${ARCH}
-akmods --force --kernels "${KERNEL}" --kmod akmod-vhba
-modinfo /usr/lib/modules/${KERNEL}/extra/akmod-vhba/akmod-vhba.ko.xz > /dev/null \
-|| (find /var/cache/akmods/akmod-vhba/ -name \*.log -print -exec cat {} \; && exit 1)
+akmods --force --kernels "${KERNEL}" --kmod vhba
+modinfo /usr/lib/modules/${KERNEL}/extra/vhba/vhba.ko.xz > /dev/null \
+|| (find /var/cache/akmods/vhba/ -name \*.log -print -exec cat {} \; && exit 1)
