71253aab5dca80dc271984cbb2c903d8b48b667b
Jon Mulder <jon.e.mulder@gmail.com>
2024-04-19 12:14:13 -0400

    chore(facetimehd): quiet curl output

--- build-kmod-facetimehd.sh
+++ build-kmod-facetimehd.sh
@@ -12,7 +12,7 @@ else
     COPR_RELEASE="${RELEASE}"
 fi
 
-curl -L "https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-${COPR_RELEASE}/mulderje-facetimehd-kmod-fedora-${COPR_RELEASE}.repo" \
+curl -LsSf "https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-${COPR_RELEASE}/mulderje-facetimehd-kmod-fedora-${COPR_RELEASE}.repo" \
      -o /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
 
 ### BUILD facetimehd (succeed or fail-fast with debug output)
