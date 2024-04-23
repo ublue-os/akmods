fedcad9f999519db469ce74b713a31a494e4d6de
Marco Rodolfi <marco.rodolfi@tuta.io>
2024-04-23 07:04:43 +0200

    feat: add Rok CDEmu for including vhba kmod (#165)
    Co-authored-by: Benjamin Sherman <benjamin@holyarmy.org>
    Co-authored-by: Kyle Gospodnetich <me@kylegospodneti.ch>

---
+++ build-kmod-vhba.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+if [[ "$RELEASE" -lt 39 ]]; then
+  echo "SKIPPED BUILD of vhba on Fedora $RELEASE"
+  exit 0
+fi
+
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_rok-cdemu.repo /etc/yum.repos.d/
+
+### BUILD vhba (succeed or fail-fast with debug output)
+rpm-ostree install \
+    akmod-vhba-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod vhba
+modinfo /usr/lib/modules/${KERNEL}/extra/vhba/vhba.ko.xz > /dev/null \
+|| (find /var/cache/akmods/vhba/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/_copr_rok-cdemu.repo
