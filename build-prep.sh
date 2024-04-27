14c920232859311f8d66a57b681b35e17280cde7
Benjamin Sherman <benjamin@holyarmy.org>
2024-04-27 02:11:53 -0500

    chore: remove Fedora 38
    This removes Fedora 38 references from the repo.
    
    Closes: ublue-os/main#527

--- build-prep.sh
+++ build-prep.sh
@@ -7,12 +7,7 @@ set -oeux pipefail
 ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 
-# Modularity repositories are not available on Fedora 39 and above, so don't try to disable them
-if [[ "${RELEASE}" -le 38 ]]; then
-    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
-else
-    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-fi
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 
 # enable RPMs with alternatives to create them in this image build
 mkdir -p /var/lib/alternatives
