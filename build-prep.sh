7bd2adb14751a8404b52154abb9b5f3e1400a5a6
Mark Mihalik <m2@m2home.mynetworksettings.com>
2024-06-24 16:01:18 -0400

    feat: support CoreOS Kernel

--- build-prep.sh
+++ build-prep.sh
@@ -106,6 +106,17 @@ elif [[ "surface" == "${KERNEL_FLAVOR}" ]]; then
         --install kernel-surface-modules \
         --install kernel-surface-modules-core \
         --install kernel-surface-modules-extra
+elif [[ "coreos" == "${KERNEL_FLAVOR}" ]] && \
+     [[ -n "${KERNEL_VERSION}" ]]; then
+    echo "Installing CoreOS Kernel"
+    KERNEL_MAJOR_MINOR_PATCH=$(echo $KERNEL_VERSION | cut -d '-' -f 1)
+    KERNEL_RELEASE=$(echo $KERNEL_VERSION | cut -d '-' -f 2)
+    rpm-ostree override replace --experimental \
+        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-core-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-modules-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-modules-core-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-modules-extra-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm
 elif [[ "main" == "${KERNEL_FLAVOR}" ]] && \
      [[ "" != "${KERNEL_VERSION}" ]]; then
     echo "main kernel version ${KERNEL_VERSION} to avoid upgrading kernel beyond what is in the image."
