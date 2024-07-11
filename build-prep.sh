b23b9b613846e520a210f8ebc7b321bb5e9256c8
Benjamin Sherman <benjamin@holyarmy.org>
2024-07-11 00:57:58 -0500

    feat: continued work to use kernel-cache for akmods
    This updates workflows to pull both the builder (fedora) and
    kernel-cache images, then inspect the locally stored kernel-cache image
    for its kernel and image versions.
    
    Containerfiles and build-prep are a bit simpler now as the kernel
    installation process is simpler than previous rpm-ostree replacement
    operations.

--- build-prep.sh
+++ build-prep.sh
@@ -12,6 +12,16 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 # enable RPMs with alternatives to create them in this image build
 mkdir -p /var/lib/alternatives
 
+# install kernel_cache provided kernel
+echo "Installing ${KERNEL_FLAVOR} kernel-cache RPMs..."
+# fedora image has no kernel so this needs nothing fancy, just install
+dnf install -y /tmp/kernel_cache/*.rpm
+if [[ "${KERNEL_FLAVOR}" == "surface" ]]; then
+    KERNEL_VERSION=$(rpm -q kernel-surface|cut -d '-' -f2-)
+else
+    KERNEL_VERSION=$(rpm -q kernel|cut -d '-' -f2-)
+fi
+
 # enable more repos
 RPMFUSION_MIRROR_RPMS="https://mirrors.rpmfusion.org"
 if [ -n "${RPMFUSION_MIRROR}" ]; then
@@ -37,106 +47,6 @@ if [ -n "${RPMFUSION_MIRROR}" ]; then
     sed -i "s%^#baseurl=http://download1.rpmfusion.org%baseurl=${RPMFUSION_MIRROR}%" /etc/yum.repos.d/rpmfusion-*.repo
 fi
 
-# Disabled/Commented as this now includs fc41 packages which are causing more harm than help
-## required for main and surface when fedora repo has updated kernel beyond what was in the image
-##curl -LsSf -o /etc/yum.repos.d/fedora-coreos-pool.repo \
-##    https://raw.githubusercontent.com/coreos/fedora-coreos-config/testing-devel/fedora-coreos-pool.repo
-
-### PREPARE CUSTOM KERNEL SUPPORT
-if [[ "asus" == "${KERNEL_FLAVOR}" ]]; then
-    echo "Installing ASUS Kernel:"
-    curl -LsSf -o /etc/yum.repos.d/_copr_lukenukem-asus-kernel.repo \
-        https://copr.fedorainfracloud.org/coprs/lukenukem/asus-kernel/repo/fedora-$(rpm -E %fedora)/lukenukem-asus-kernel-fedora-$(rpm -E %fedora).repo
-    rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:lukenukem:asus-kernel \
-        kernel \
-        kernel-core \
-        kernel-devel \
-        kernel-devel-matched \
-        kernel-modules \
-        kernel-modules-core \
-        kernel-modules-extra
-elif [[ "fsync-lts" == "${KERNEL_FLAVOR}" ]]; then
-    echo "Installing fsync-lts kernel:"
-    curl -LsSf -o /etc/yum.repos.d/_copr_sentry-kernel-ba.repo \
-        https://copr.fedorainfracloud.org/coprs/sentry/kernel-ba/repo/fedora-$(rpm -E %fedora)/sentry-kernel-ba-fedora-$(rpm -E %fedora).repo
-    rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:sentry:kernel-ba \
-        kernel \
-        kernel-core \
-        kernel-devel \
-        kernel-devel-matched \
-        kernel-modules \
-        kernel-modules-core \
-        kernel-modules-extra
-elif [[ "fsync" == "${KERNEL_FLAVOR}" ]]; then
-    echo "Installing fsync kernel:"
-    curl -LsSf -o /etc/yum.repos.d/_copr_sentry-kernel-fsync.repo \
-        https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-$(rpm -E %fedora)/sentry-kernel-fsync-fedora-$(rpm -E %fedora).repo
-    rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:sentry:kernel-fsync \
-        kernel \
-        kernel-core \
-        kernel-devel \
-        kernel-devel-matched \
-        kernel-modules \
-        kernel-modules-core \
-        kernel-modules-extra
-elif [[ "surface" == "${KERNEL_FLAVOR}" ]]; then
-    echo "Installing Surface Kernel:"
-    # Add Linux Surface repo
-    curl -LsSf -o /etc/yum.repos.d/linux-surface.repo \
-        https://pkg.surfacelinux.com/fedora/linux-surface.repo
-    curl -LsSf -o /tmp/surface-kernel.rpm \
-        https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
-    rpm-ostree override replace /tmp/surface-kernel.rpm \
-        --remove kernel-core \
-        --remove kernel-modules \
-        --remove kernel-modules-extra \
-        --install kernel-surface \
-        --install kernel-surface-core \
-        --install kernel-surface-devel \
-        --install kernel-surface-devel-matched \
-        --install kernel-surface-modules \
-        --install kernel-surface-modules-core \
-        --install kernel-surface-modules-extra
-elif [[ "${KERNEL_FLAVOR}" =~ "coreos" ]] && \
-     [[ -n "${KERNEL_VERSION}" ]]; then
-    echo "Installing CoreOS Kernel"
-    KERNEL_MAJOR_MINOR_PATCH=$(echo $KERNEL_VERSION | cut -d '-' -f 1)
-    KERNEL_RELEASE=$(echo $KERNEL_VERSION | cut -d '-' -f 2)
-    rpm-ostree override replace --experimental \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-core-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-modules-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-modules-core-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-modules-extra-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-devel-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm \
-        https://kojipkgs.fedoraproject.org//packages/kernel/$KERNEL_MAJOR_MINOR_PATCH/$KERNEL_RELEASE/x86_64/kernel-devel-matched-$KERNEL_MAJOR_MINOR_PATCH-$KERNEL_RELEASE.x86_64.rpm && \
-    dnf install -y \
-        autoconf \
-        automake \
-        dkms \
-        git \
-        libtool \
-        ncompress \
-        dnf
-elif [[ "main" == "${KERNEL_FLAVOR}" ]] && \
-     [[ "" != "${KERNEL_VERSION}" ]]; then
-    echo "main kernel version ${KERNEL_VERSION} to avoid upgrading kernel beyond what is in the image."
-    dnf install -y \
-        kernel-devel-${KERNEL_VERSION} \
-        kernel-devel-matched-${KERNEL_VERSION}
-else
-    echo "Default main kernel without a specific version."
-    dnf install -y \
-        kernel-devel \
-        kernel-devel-matched
-fi
-
 ### PREPARE BUILD ENV
 dnf install -y \
     akmods \
@@ -151,9 +61,18 @@ fi
 install -Dm644 /tmp/certs/public_key.der   /etc/pki/akmods/certs/public_key.der
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
+# This is for ZFS more than CoreOS
 if [[ "${KERNEL_FLAVOR}" =~ "coreos" ]]; then
-    install -Dm644 /tmp/certs/public_key.der /lib/modules/${KERNEL_VERSION}.x86_64/build/certs/signing_key.x509
-    install -Dm644 /tmp/certs/private_key.priv /lib/modules/${KERNEL_VERSION}.x86_64/build/certs/signing_key.pem
+    install -Dm644 /tmp/certs/public_key.der /lib/modules/${KERNEL_VERSION}/build/certs/signing_key.x509
+    install -Dm644 /tmp/certs/private_key.priv /lib/modules/${KERNEL_VERSION}/build/certs/signing_key.pem
+    dnf install -y \
+        autoconf \
+        automake \
+        dkms \
+        git \
+        libtool \
+        ncompress \
+        dnf
 fi
 
 # protect against incorrect permissions in tmp dirs which can break akmods builds
