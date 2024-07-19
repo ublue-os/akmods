20fce9fe76de494ff901f017a50d6f4dfc1661e0
m2 <69128853+m2Giles@users.noreply.github.com>
2024-07-18 21:43:15 -0400

    feat: Enable Dual Signing for akmods (#215)

--- build-prep.sh
+++ build-prep.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 set -oeux pipefail
 
 
 ### PREPARE REPOS
-ARCH="$(rpm -E '%_arch')"
+# ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
@@ -28,8 +28,8 @@ if [ -n "${RPMFUSION_MIRROR}" ]; then
     RPMFUSION_MIRROR_RPMS=${RPMFUSION_MIRROR}
 fi
 dnf install -y \
-    ${RPMFUSION_MIRROR_RPMS}/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-    ${RPMFUSION_MIRROR_RPMS}/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
+    "${RPMFUSION_MIRROR_RPMS}"/free/fedora/rpmfusion-free-release-"${RELEASE}".noarch.rpm \
+    "${RPMFUSION_MIRROR_RPMS}"/nonfree/fedora/rpmfusion-nonfree-release-"${RELEASE}".noarch.rpm \
     fedora-repos-archive
 
 # after F41 launches, bump to 42
@@ -61,10 +61,24 @@ fi
 install -Dm644 /tmp/certs/public_key.der   /etc/pki/akmods/certs/public_key.der
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
+if [[ "${DUAL_SIGN}" == "true" ]]; then
+    dnf install -y rpmrebuild
+    if [[ ! -s "/tmp/certs/private_key_2.priv" ]]; then
+        echo "WARNING: Using test signing key. Run './generate-akmods-key' for production builds."
+        cp /tmp/certs/private_key_2.priv{.test,}
+        cp /tmp/certs/public_key_2.der{.test,}
+    fi
+    openssl x509 -in /tmp/certs/public_key_2.der -out /tmp/certs/public_key_2.crt
+    openssl x509 -in /tmp/certs/public_key.der -out /tmp/certs/public_key.crt
+    cat /tmp/certs/private_key.priv <(echo) /tmp/certs/public_key.crt >> /tmp/certs/signing_key_1.pem
+    cat /tmp/certs/private_key_2.priv <(echo) /tmp/certs/public_key_2.crt >> /tmp/certs/signing_key_2.pem
+    cat /tmp/certs/public_key.crt <(echo) /tmp/certs/public_key_2.crt >> /tmp/certs/public_key_chain.pem
+fi
+
 # This is for ZFS more than CoreOS
 if [[ "${KERNEL_FLAVOR}" =~ "coreos" ]]; then
-    install -Dm644 /tmp/certs/public_key.der /lib/modules/${KERNEL_VERSION}/build/certs/signing_key.x509
-    install -Dm644 /tmp/certs/private_key.priv /lib/modules/${KERNEL_VERSION}/build/certs/signing_key.pem
+    install -Dm644 /tmp/certs/public_key.der /lib/modules/"${KERNEL_VERSION}"/build/certs/signing_key.x509
+    install -Dm644 /tmp/certs/private_key.priv /lib/modules/"${KERNEL_VERSION}"/build/certs/signing_key.pem
     dnf install -y \
         autoconf \
         automake \
---
+++ dual-sign-zfs.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+SIGNING_KEY_1="/tmp/certs/signing_key_1.pem"
+SIGNING_KEY_2="/tmp/certs/signing_key_2.pem"
+PUBLIC_CHAIN="/tmp/certs/public_key_chain.pem"
+
+if [[ "${DUAL_SIGN}" == "true" ]]; then
+    dnf install -y /var/cache/rpms/kmods/zfs/*.rpm pv
+    modinfo /usr/lib/modules/"${KERNEL}"/extra/zfs/spl.ko
+    for module in /usr/lib/modules/"${KERNEL}"/extra/zfs/*.ko*;
+    do
+        module_basename=${module:0:-3}
+        module_suffix=${module: -3}
+        if [[ "$module_suffix" == ".xz" ]]; then
+                xz --decompress "$module"
+                openssl cms -sign -signer "${SIGNING_KEY_1}" -signer "${SIGNING_KEY_2}" -binary -in "$module_basename" -outform DER -out "${module_basename}.cms" -nocerts -noattr -nosmimecap
+                /usr/src/kernels/"${KERNEL}"/scripts/sign-file -s "${module_basename}.cms" sha256 "${PUBLIC_CHAIN}" "${module_basename}"
+                xz -f "${module_basename}"
+        elif [[ "$module_suffix" == ".gz" ]]; then
+                gzip -d "$module"
+                openssl cms -sign -signer "${SIGNING_KEY_1}" -signer "${SIGNING_KEY_2}" -binary -in "$module_basename" -outform DER -out "${module_basename}.cms" -nocerts -noattr -nosmimecap
+                /usr/src/kernels/"${KERNEL}"/scripts/sign-file -s "${module_basename}.cms" sha256 "${PUBLIC_CHAIN}" "${module_basename}"
+                gzip -9f "${module_basename}"
+        else
+                openssl cms -sign -signer "${SIGNING_KEY_1}" -signer "${SIGNING_KEY_2}" -binary -in "$module" -outform DER -out "${module}.cms" -nocerts -noattr -nosmimecap
+                /usr/src/kernels/"${KERNEL}"/scripts/sign-file -s "${module}.cms" sha256 "${PUBLIC_CHAIN}" "${module}"
+        fi
+    done
+    rpmrebuild --batch /var/cache/rpms/kmods/zfs/kmod-zfs-*.rpm
+    rm -rf /usr/lib/modules/"${KERNEL}"/extra
+    dnf reinstall -y /root/rpmbuild/RPMS/"$(uname -m)"/kmod-*-"${KERNEL}"-*.rpm
+    for module in /usr/lib/modules/"${KERNEL}"/extra/*/*.ko*; do
+        if ! modinfo "${module}"; then
+            exit 1
+        fi
+    done
+    mv -f /root/rpmbuild/RPMS/"$(uname -m)"/kmod-*.rpm /var/cache/rpms/kmods/zfs/
+fi
+
+for RPM in $(find /var/cache/akmods/ -type f -name \*.rpm); do
+    cp "${RPM}" /var/cache/rpms/kmods/
+done
+
+find /var/cache/rpms
