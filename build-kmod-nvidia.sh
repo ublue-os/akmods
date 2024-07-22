b24a83bc79f44a35da6d71de11c9635ee327184c
Robert Sturla <robertsturla@outlook.com>
2024-07-22 09:54:08 +0100

    chore: output license information

--- build-kmod-nvidia.sh
+++ build-kmod-nvidia.sh
@@ -27,6 +27,8 @@ akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
 modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
 (cat /var/cache/akmods/nvidia/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
 
+# View license information
+modinfo -l /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz
 
 # create a directory for later copying of resulting nvidia specific artifacts
 mkdir -p /var/cache/rpms/kmods/nvidia
