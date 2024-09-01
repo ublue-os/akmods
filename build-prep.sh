ee7dfa74b0da717eb841c6a118769d875df5dbc3
Benjamin Sherman <benjamin@holyarmy.org>
2024-08-31 22:38:23 -0500

    fsync-coreos doens't need a change to ZFS logic

--- build-prep.sh
+++ build-prep.sh
@@ -75,7 +75,7 @@ if [[ "${DUAL_SIGN}" == "true" ]]; then
 fi
 
 # This is for ZFS more than CoreOS
-if [[ "${KERNEL_FLAVOR}" =~ coreos|gated ]]; then
+if [[ "${KERNEL_FLAVOR}" =~ "coreos" ]]; then
     install -Dm644 /tmp/certs/public_key.der /lib/modules/"${KERNEL_VERSION}"/build/certs/signing_key.x509
     install -Dm644 /tmp/certs/private_key.priv /lib/modules/"${KERNEL_VERSION}"/build/certs/signing_key.pem
     dnf install -y \
