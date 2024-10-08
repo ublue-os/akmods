e4c1c25f60ae23aa214112b9420d4e3583962c00
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-10-07 23:06:15 -0400

    chore: fix codacy warning

--- build-kmod-evdi.sh
+++ build-kmod-evdi.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -oux pipefail
-
+set -eoux pipefail
 
 ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
@@ -10,7 +9,7 @@ RELEASE="$(rpm -E '%fedora')"
 cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/negativo17-fedora-multimedia.repo /etc/yum.repos.d/
 
 if [[ "${FEDORA_MAJOR_VERSION}" -ge 41 ]]; then
-  if dnf search displaylink|grep -qv "displaylink"; then
+  if dnf search displaylink | grep -qv "displaylink"; then
     echo "Skipping build of evdi; displaylink net yet provided by negativo17"
     exit 0
   fi
@@ -21,10 +20,10 @@ set -e pipefail
 ### BUILD evdi (succeed or fail-fast with debug output)
 export CFLAGS="-fno-pie -no-pie"
 dnf install -y \
-    kmod-evdi*.fc${RELEASE}.${ARCH} akmod-evdi-*.fc${RELEASE}.${ARCH}
+  "kmod-evdi*.fc${RELEASE}.${ARCH} akmod-evdi-*.fc${RELEASE}.${ARCH}"
 akmods --force --kernels "${KERNEL}" --kmod evdi
-modinfo /usr/lib/modules/${KERNEL}/extra/evdi/evdi.ko.xz > /dev/null \
-|| (find /var/cache/akmods/evdi/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/"${KERNEL}"/extra/evdi/evdi.ko.xz >/dev/null ||
+  (find /var/cache/akmods/evdi/ -name \*.log -print -exec cat {} \; && exit 1)
 
 rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 unset CFLAGS
