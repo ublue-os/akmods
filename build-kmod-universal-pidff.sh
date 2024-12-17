e1fd4b0701d977424fdd9a4ceacf07ba4c67cadc
J.P. Zivalich <jp@pipekit.io>
2024-12-17 12:13:15 +0100

    fix: Add quotes
    Signed-off-by: J.P. Zivalich <jp@pipekit.io>

--- build-kmod-universal-pidff.sh
+++ build-kmod-universal-pidff.sh
@@ -9,9 +9,9 @@ RELEASE="$(rpm -E '%fedora')"
 cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
 
 dnf install -y \
-    akmod-universal-pidff-*.fc${RELEASE}.${ARCH}
+    akmod-universal-pidff-*.fc"${RELEASE}.${ARCH}"
 akmods --force --kernels "${KERNEL}" --kmod universal-pidff
-modinfo /usr/lib/modules/${KERNEL}/extra/universal-pidff/universal-pidff.ko.xz > /dev/null \
+modinfo /usr/lib/modules/"${KERNEL}"/extra/universal-pidff/universal-pidff.ko.xz > /dev/null \
 || (find /var/cache/akmods/universal-pidff/ -name \*.log -print -exec cat {} \; && exit 1)
 
 rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
