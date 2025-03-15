fa2fdc32dec91c126325990360b29ddef2847e73
Robert Sturla <robertsturla@outlook.com>
2025-03-15 19:26:56 +0000

    chore: remove openrazer in F42

--- build_files/common/build-kmod-openrazer.sh
+++ build_files/common/build-kmod-openrazer.sh
@@ -6,19 +6,24 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+# OpenRazer builds are broken on Fedora 42
+if [[ "${FEDORA_MAJOR_VERSION}" -eq 42 ]]; then
+    exit 0
+fi
+
 cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
 
 ### BUILD openrazer (succeed or fail-fast with debug output)
 dnf install -y \
     akmod-openrazer-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod openrazer
-modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razerkbd.ko.xz > /dev/null \
-|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
-modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razermouse.ko.xz > /dev/null \
-|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
-modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razerkraken.ko.xz > /dev/null \
-|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
-modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razeraccessory.ko.xz > /dev/null \
-|| (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razerkbd.ko.xz >/dev/null ||
+    (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razermouse.ko.xz >/dev/null ||
+    (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razerkraken.ko.xz >/dev/null ||
+    (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/openrazer/razeraccessory.ko.xz >/dev/null ||
+    (find /var/cache/akmods/openrazer/ -name \*.log -print -exec cat {} \; && exit 1)
 
 rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
