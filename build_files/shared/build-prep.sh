2b68d72f37510c7e487b9f4953c46d8128cd170c
Benjamin Sherman <benjamin@holyarmy.org>
2025-05-10 17:30:53 -0500

    feat: add centos 10 support (#322)
    * feat: add centos 10 support
    
    * use version 10
    
    * get kernel version with correct tag
    
    * trying to get it to work
    
    * temp disable extra workflows
    
    * use same CentOS tag as bluefin LTS
    
    * disable SB sign/verify for centos
    
    * centos version for downloads
    
    * don't fail if didn't rebuild rpms
    
    * matrix exclude all but zfs for centos
    
    * add EPEL and re-add rpmrebuild/sbsigntools
    
    * use actual centos version
    
    * pull correct build image
    
    * don't double stream
    
    * missing end quotes
    
    * snapshot of progress
    
    * only attempt to install kernel RPMs from kernel cache
    
    * need kernel-devel for CentOS
    
    * Bump to ZFS 2.3 since ZFS 2.2.7 not building with CentOS10's default kernel
    
    * add python-cffi
    
    * skip secureboot checks for centos
    
    * fix quotes
    
    * should build stream 9 and 10
    
    * initial test hyperscale
    
    * trigger build?
    
    * don't try hyperscale stream9
    
    * typo
    
    * someday
    
    * centosy things
    
    * i expect centos-hsk
    
    * download hsk kernel with dnf
    
    * need repo to download
    
    * yep
    
    * attempt signing and verification with CentOS
    
    * re-enable 41, 42, bazzite
    
    ---------
    
    Co-authored-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/shared/build-prep.sh
+++ build_files/shared/build-prep.sh
@@ -2,46 +2,57 @@
 
 set -oeux pipefail
 
-
 ### PREPARE REPOS
-# ARCH="$(rpm -E '%_arch')"
-RELEASE="$(rpm -E '%fedora')"
-
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-
 # enable RPMs with alternatives to create them in this image build
 mkdir -p /var/lib/alternatives
 
+# ARCH="$(rpm -E '%_arch')"
+if [[ "${KERNEL_FLAVOR}" =~ "centos" ]]; then
+    echo "Building for CentOS"
+    RELEASE="$(rpm -E '%centos')"
+
+    mkdir -p /var/roothome
+
+    dnf remove -y subscription-manager
+    dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${RELEASE}.noarch.rpm"
+    dnf config-manager --set-enabled crb
+else
+    echo "Building for Fedora"
+    RELEASE="$(rpm -E '%fedora')"
+
+    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+    RPMFUSION_MIRROR_RPMS="https://mirrors.rpmfusion.org"
+    if [ -n "${RPMFUSION_MIRROR}" ]; then
+        RPMFUSION_MIRROR_RPMS=${RPMFUSION_MIRROR}
+    fi
+    dnf install -y \
+        "${RPMFUSION_MIRROR_RPMS}"/free/fedora/rpmfusion-free-release-"${RELEASE}".noarch.rpm \
+        "${RPMFUSION_MIRROR_RPMS}"/nonfree/fedora/rpmfusion-nonfree-release-"${RELEASE}".noarch.rpm \
+        fedora-repos-archive
+
+    # after F42 launches, bump to 43
+    if [[ "${FEDORA_MAJOR_VERSION}" -ge 42 ]]; then
+        # pre-release rpmfusion is in a different location
+        sed -i "s%free/fedora/releases%free/fedora/development%" /etc/yum.repos.d/rpmfusion-*.repo
+        # pre-release rpmfusion needs to enable testing
+        sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/rpmfusion-*-updates-testing.repo
+    fi
+
+    if [ -n "${RPMFUSION_MIRROR}" ]; then
+        # force use of single rpmfusion mirror
+        echo "Using single rpmfusion mirror: ${RPMFUSION_MIRROR}"
+        sed -i.bak "s%^metalink=%#metalink=%" /etc/yum.repos.d/rpmfusion-*.repo
+        sed -i "s%^#baseurl=http://download1.rpmfusion.org%baseurl=${RPMFUSION_MIRROR}%" /etc/yum.repos.d/rpmfusion-*.repo
+    fi
+fi
+
 # install kernel_cache provided kernel
 echo "Installing ${KERNEL_FLAVOR} kernel-cache RPMs..."
-# fedora image has no kernel so this needs nothing fancy, just install
-dnf install -y /tmp/kernel_cache/*.rpm
+
+# build image has no kernel so this needs nothing fancy, just install, but not UKIs
+dnf install -y `find /tmp/kernel_cache/*.rpm -type f | grep -v uki | xargs`
 KERNEL_VERSION=$(rpm -q "${KERNEL_NAME}" | cut -d '-' -f2-)
-
-# enable more repos
-RPMFUSION_MIRROR_RPMS="https://mirrors.rpmfusion.org"
-if [ -n "${RPMFUSION_MIRROR}" ]; then
-    RPMFUSION_MIRROR_RPMS=${RPMFUSION_MIRROR}
-fi
-dnf install -y \
-    "${RPMFUSION_MIRROR_RPMS}"/free/fedora/rpmfusion-free-release-"${RELEASE}".noarch.rpm \
-    "${RPMFUSION_MIRROR_RPMS}"/nonfree/fedora/rpmfusion-nonfree-release-"${RELEASE}".noarch.rpm \
-    fedora-repos-archive
-
-# after F42 launches, bump to 43
-if [[ "${FEDORA_MAJOR_VERSION}" -ge 42 ]]; then
-    # pre-release rpmfusion is in a different location
-    sed -i "s%free/fedora/releases%free/fedora/development%" /etc/yum.repos.d/rpmfusion-*.repo
-    # pre-release rpmfusion needs to enable testing
-    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/rpmfusion-*-updates-testing.repo
-fi
-
-if [ -n "${RPMFUSION_MIRROR}" ]; then
-    # force use of single rpmfusion mirror
-    echo "Using single rpmfusion mirror: ${RPMFUSION_MIRROR}"
-    sed -i.bak "s%^metalink=%#metalink=%" /etc/yum.repos.d/rpmfusion-*.repo
-    sed -i "s%^#baseurl=http://download1.rpmfusion.org%baseurl=${RPMFUSION_MIRROR}%" /etc/yum.repos.d/rpmfusion-*.repo
-fi
 
 ### PREPARE BUILD ENV
 dnf install -y \
@@ -74,8 +85,8 @@ if [[ "${DUAL_SIGN}" == "true" ]]; then
     cat /tmp/certs/public_key.crt <(echo) /tmp/certs/public_key_2.crt >> /tmp/certs/public_key_chain.pem
 fi
 
-# This is for ZFS more than CoreOS
-if [[ "${KERNEL_FLAVOR}" =~ "coreos" ]]; then
+# This is for ZFS more than CentOS|CoreOS
+if [[ "${KERNEL_FLAVOR}" =~ "centos" ]] || [[ "${KERNEL_FLAVOR}" =~ "coreos" ]]; then
     install -Dm644 /tmp/certs/public_key.der /lib/modules/"${KERNEL_VERSION}"/build/certs/signing_key.x509
     install -Dm644 /tmp/certs/private_key.priv /lib/modules/"${KERNEL_VERSION}"/build/certs/signing_key.pem
     dnf install -y \
