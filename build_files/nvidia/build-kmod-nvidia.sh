d4bdf34bc93696eed8383e962c5019ec7d568c5b
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-10 13:49:24 -0500

    fixes for those release vars

--- build_files/nvidia/build-kmod-nvidia.sh
+++ build_files/nvidia/build-kmod-nvidia.sh
@@ -2,13 +2,15 @@
 
 set -oeux pipefail
 
-DIST_ARCH="$(rpm -E '%fedora.%_arch')"
+DIST_ARCH="$(rpm -E '%dist.%_arch')"
 KERNEL_MODULE_TYPE="${1:-kernel}"
 
 if [[ "${KERNEL_FLAVOR}" =~ "centos" ]]; then
+    RELEASE_ARCH="$(rpm -E '%centos%_arch')"
     # enable negativo17
     cp /tmp/ublue-os-nvidia-addons/rpmbuild/SOURCES/negativo17-epel-nvidia.repo /etc/yum.repos.d/
 else
+    RELEASE_ARCH="$(rpm -E '%fedora%_arch')"
     # disable rpmfusion and enable negativo17
     sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/rpmfusion-*.repo
     cp /tmp/ublue-os-nvidia-addons/rpmbuild/SOURCES/negativo17-fedora-nvidia.repo /etc/yum.repos.d/
@@ -44,6 +46,6 @@ mkdir -p /var/cache/rpms/kmods/nvidia
 cat <<EOF > /var/cache/rpms/kmods/nvidia-vars
 KERNEL_VERSION=${KERNEL_VERSION}
 KERNEL_MODULE_TYPE=${KERNEL_MODULE_TYPE}
-RELEASE="$(rpm -E '%fedora.%_arch')"
+RELEASE=${RELEASE_ARCH}
 NVIDIA_AKMOD_VERSION=${NVIDIA_AKMOD_VERSION}
 EOF
