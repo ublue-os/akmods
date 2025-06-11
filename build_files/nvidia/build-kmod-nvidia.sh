ad2ffdd6994b38adad5566901115b906613247b4
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-10 21:38:38 -0500

    fix an error in handing basename operation with DIST_RELEASE

--- build_files/nvidia/build-kmod-nvidia.sh
+++ build_files/nvidia/build-kmod-nvidia.sh
@@ -2,15 +2,17 @@
 
 set -oeux pipefail
 
-DIST_ARCH="$(rpm -E '%dist.%_arch')"
+ARCH=$(rpm -E %_arch)
 KERNEL_MODULE_TYPE="${1:-kernel}"
 
 if [[ "${KERNEL_FLAVOR}" =~ "centos" ]]; then
-    RELEASE_ARCH="$(rpm -E '%centos.%_arch')"
+    RELEASE="$(rpm -E '%centos')"
+    DIST_RELEASE="el${RELEASE}"
     # enable negativo17
     cp /tmp/ublue-os-nvidia-addons/rpmbuild/SOURCES/negativo17-epel-nvidia.repo /etc/yum.repos.d/
 else
-    RELEASE_ARCH="$(rpm -E '%fedora.%_arch')"
+    RELEASE="fc$(rpm -E '%fedora')"
+    DIST_RELEASE="el${RELEASE}"
     # disable rpmfusion and enable negativo17
     sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/rpmfusion-*.repo
     cp /tmp/ublue-os-nvidia-addons/rpmbuild/SOURCES/negativo17-fedora-nvidia.repo /etc/yum.repos.d/
@@ -22,12 +24,12 @@ cd /tmp
 
 
 dnf install -y \
-    akmod-nvidia*${DIST_ARCH}
+    akmod-nvidia*.${DIST_RELEASE}.${ARCH}
 
 # Either successfully build and install the kernel modules, or fail early with debug output
 rpm -qa |grep nvidia
 KERNEL_VERSION="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" "${DIST_ARCH%%.*}")"
+NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" ".${DIST_RELEASE}")"
 
 sed -i "s/^MODULE_VARIANT=.*/MODULE_VARIANT=$KERNEL_MODULE_TYPE/" /etc/nvidia/kernel.conf
 
@@ -46,6 +48,6 @@ mkdir -p /var/cache/rpms/kmods/nvidia
 cat <<EOF > /var/cache/rpms/kmods/nvidia-vars
 KERNEL_VERSION=${KERNEL_VERSION}
 KERNEL_MODULE_TYPE=${KERNEL_MODULE_TYPE}
-RELEASE=${RELEASE_ARCH}
+RELEASE="${RELEASE}.${ARCH}"
 NVIDIA_AKMOD_VERSION=${NVIDIA_AKMOD_VERSION}
 EOF
