d01fe7e0d1edbf0a2319a956e6624090b9d9b81e
m2 <69128853+m2Giles@users.noreply.github.com>
2025-07-08 02:38:02 +0000

    revert: Revert lint cleanup for fpm kmod renaming for now.
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/post/build-post.sh
+++ build_files/post/build-post.sh
@@ -14,12 +14,11 @@ popd
 # FIXME: The sed is a gross hack, maybe PR upstream?
 sed -i -e 's/args = \["rpmbuild", "-bb"\]/args = \["rpmbuild", "-bb", "--buildroot", "#{build_path}\/BUILD"\]/g' /usr/local/share/gems/gems/fpm-*/lib/fpm/package/rpm.rb
 kernel_version=$(rpm -q --qf "%{VERSION}-%{RELEASE}.%{ARCH}\n" "${KERNEL_NAME}" | head -n 1)
-mapfile -t RPMS < <(find /var/cache/rpms/kmods -type f -name \*.rpm)
-for rpm in "${RPM[@]}"; do
-    basename=$(basename "${rpm}")
-    name=${basename%%-"${kernel_version}"*}
+for rpm in $(find /var/cache/rpms/kmods -type f -name \*.rpm); do
+    basename=$(basename ${rpm})
+    name=${basename%%-${kernel_version}*}
     if [[ "$basename" == *"$kernel_version"* ]]; then
-        fpm --verbose -s rpm -t rpm -p "${rpm}" -f --name "${name}" "${rpm}"
+        fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm}
     else
         echo "Skipping $basename rebuild as its name does not contain $kernel_version"
     fi
