aba8c100dc6b986cd0fc7ffccab01c18da55783b
Kyle Gospodnetich <me@kylegospodneti.ch>
2026-03-15 23:05:28 -0700

    feat: restore xpadneo akmod

---
+++ build_files/common/build-kmod-xpadneo.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/negativo17-fedora-multimedia.repo /etc/yum.repos.d/
+
+### BUILD xpadneo
+dnf install -y \
+    akmod-xpadneo-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod xpadneo
+modinfo /usr/lib/modules/${KERNEL}/extra/xpadneo/hid-xpadneo.ko.xz > /dev/null \
+|| (find /var/cache/akmods/xpadneo/ -name \*.log -print -exec cat {} \; && exit 1)
+
+mkdir -p /var/cache/rpms/common
+dnf download --destdir /var/cache/rpms/common \
+    xpadneo-kmod-common
+
+rm -f /var/cache/rpms/common/*.src.rpm
+
+rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
