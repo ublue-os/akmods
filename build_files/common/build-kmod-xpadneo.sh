6d0145b5aae7d410feff06b75d11b25ac6c87bf2
Kyle Gospodnetich <me@kylegospodneti.ch>
2026-04-04 09:34:31 -0700

    fix: Restore the old xpadneo (#492)

--- build_files/common/build-kmod-xpadneo.sh
+++ build_files/common/build-kmod-xpadneo.sh
@@ -6,10 +6,7 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/terra.repo /etc/yum.repos.d/
-curl -LsSf -o /etc/pki/rpm-gpg/RPM-GPG-KEY-terra"${RELEASE}" \
-    "https://raw.githubusercontent.com/terrapkg/packages/f${RELEASE}/anda/terra/gpg-keys/RPM-GPG-KEY-terra${RELEASE}"
-rpmkeys --import /etc/pki/rpm-gpg/RPM-GPG-KEY-terra"${RELEASE}"
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/negativo17-fedora-multimedia.repo /etc/yum.repos.d/
 
 ### BUILD xpadneo
 dnf install -y \
@@ -24,4 +21,4 @@ dnf download --destdir /var/cache/rpms/common \
 
 rm -f /var/cache/rpms/common/*.src.rpm
 
-rm -f /etc/yum.repos.d/terra.repo
+rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
