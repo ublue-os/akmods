9ecbec60e94482811ce3cb3063534599432ab0ab
Kyle Gospodnetich <me@kylegospodneti.ch>
2026-06-04 15:21:23 -0700

    chore: Switch to terra for newer ryzen-smu

--- build_files/extra/build-kmod-ryzen-smu.sh
+++ build_files/extra/build-kmod-ryzen-smu.sh
@@ -6,7 +6,10 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/terra.repo /etc/yum.repos.d/
+curl -LsSf -o /etc/pki/rpm-gpg/RPM-GPG-KEY-terra"${RELEASE}" \
+    "https://raw.githubusercontent.com/terrapkg/packages/f${RELEASE}/anda/terra/gpg-keys/RPM-GPG-KEY-terra${RELEASE}"
+rpmkeys --import /etc/pki/rpm-gpg/RPM-GPG-KEY-terra"${RELEASE}"
 
 ### BUILD ryzen-smu (succeed or fail-fast with debug output)
 dnf install -y \
@@ -17,8 +20,9 @@ modinfo /usr/lib/modules/"${KERNEL}"/extra/ryzen-smu/ryzen_smu.ko.xz > /dev/null
 
 mkdir -p /var/cache/rpms/extra
 dnf download --destdir /var/cache/rpms/extra \
-    ryzen-smu
+    ryzen-smu \
+    ryzen-smu-akmod-modules
 
 rm -f /var/cache/rpms/extra/*.src.rpm
 
-rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+rm -f /etc/yum.repos.d/terra.repo
